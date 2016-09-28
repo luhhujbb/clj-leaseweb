3523284f381fde49e174643edac8b5675233f721
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2016-09-28 15:37:26 +0200

    [V1] snapshot, build pass , not tested

--- src/leaseweb/core.clj
+++ src/leaseweb/core.clj
@@ -1,4 +1,9 @@
-(ns leaseweb.core)
+(ns leaseweb.core
+  (:require [clojure.string :as str]
+            [clojure.tools.logging :as log]
+            [cheshire.core :refer :all]
+            [clj-http.client :as http]
+            [digest]))
 
 (def creds (atom {}))
 (def initialized (atom false))
@@ -14,3 +19,58 @@
                    :throw-exceptions false})
 
 (defn mk-headers [] {"X-Lsw-Auth" (:token @creds)})
+
+(defn validate
+  ([res code]
+    (validate res code nil))
+  ([res code fallback-value]
+  (if (= code (:status res))
+    (:body res)
+    fallback-value)))
+
+    
+(defmulti call (fn [params] (:method params)))
+
+(defmethod call "GET" [params]
+  (let [url (str endpoint (:resource params))
+        opts (merge {:headers (mk-headers) :accept :json :as :json} request-conf)]
+        (try
+          (http/get url opts)
+          (catch Exception e
+            (log/error "Ressource : "url "- Error :" e)
+            {:status 500}))))
+
+(defmethod call "PUT" [params]
+  (let [url (str endpoint (:resource params))
+        opts (merge {:form-params (:body params) :headers (mk-headers)} request-conf)]
+      (try
+        (http/post url opts)
+        (catch Exception e
+          (log/error "Ressource : "url "- Error :" e)
+          {:status 500}))))
+
+(defmethod call "POST" [params]
+  (let [url (str endpoint (:resource params))
+        opts (merge {:form-params (:body params) :headers (mk-headers)} request-conf)]
+      (try
+        (http/post url opts)
+        (catch Exception e
+          (log/error "Ressource : "url "- Error :" e)
+          {:status 500}))))
+
+(defmethod call "DELETE" [params]
+ (let [url (str endpoint (:resource params))
+       opts (merge {:headers (mk-headers)} request-conf)]
+    (try
+      (http/delete url opts)
+      (catch Exception e
+        (log/error "Ressource : "url "- Error :" e)
+        {:status 500}))))
+
+(defmethod call :default [params]
+  (log/info "Unsupported http verb"))
+
+(defn init!
+  [token]
+  (swap! creds assoc :token token)
+  (reset! initialized true))
