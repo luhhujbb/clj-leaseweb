c60b28ac0aa3bb82af6c3b533aec5ca531a52a32
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2016-10-03 15:14:31 +0200

    [Version] remove snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject leaseweb "0.1.0-SNAPSHOT"
+(defproject leaseweb "0.1.0"
   :description "A clojure lib to interact with leaseweb dedicated server API"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
