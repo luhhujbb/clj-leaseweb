e72ce1b73b13e2748d2b0f9de60b9eb406ad796b
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2018-04-09 12:12:04 +0200

    [SNAPSHOT]

--- src/leaseweb/v2/server.clj
+++ src/leaseweb/v2/server.clj
@@ -40,112 +40,100 @@
              :body {:reference reference}}))
 
 
-;;not yet updated
-
 (defn suggested-raid-configuration
-  [client server-description]
-  (let [disks-conf (get-in server-description [:server :hardDisks] nil)]
-        (if-let [match (get (re-matches #"([0-9]+)x.*" disks-conf) 1)]
-          (let [disk-nb (Long/parseLong match)]
+  [server-description]
+  (let [disks-conf (get-in server-description [:specs :hdd 0] nil)]
+          (let [disk-nb (:amount disks-conf)]
             {:number-disks disk-nb
              :raid-level (cond
                             (= 2 disk-nb) 1
                             (= 3 disk-nb) 5
                             (= 4 disk-nb) 5
                             (> disk-nb 4) 6
-                            :else nil)})
-          nil)))
+                            :else nil)})))
+
+(defn power-status
+    [client server-id]
+    (:pdu (l/validate
+        (l/call client {:method "GET"
+            :resource (str api-path "/" server-id "/powerInfo")})
+            200 {:pdu nil})))
 
 (defn ips
   [client server-id]
   (:ips (l/validate
       (l/call client {:method "GET"
                :resource (str api-path "/" server-id "/ips")})
-               200 {:ips []})))
+               200 {:ips nil})))
 
-(defn power-status
-  [client server-id]
-  (:powerStatus (l/validate
+(defn get-os-user
+    [client server-id user]
+    (l/validate
       (l/call client {:method "GET"
-               :resource (str api-path "/" server-id "/powerStatus")})
-               200 {:powerStatus nil})))
-
-(defn open-switchport
-  [client server-id]
-  (l/validate
-      (l/call client {:method "POST"
-               :resource (str api-path "/" server-id "/switchPort/open")})
-               200 ))
-
-(defn close-switchport
-  [client server-id]
-  (l/validate
-      (l/call client {:method "POST"
-               :resource (str api-path "/" server-id "/switchPort/close")})
-                200 ))
-
-(defn network-usage
-  [client server-id date-from date-to]
-   (l/validate
-      (l/call client {:method "GET"
-               :resource (str api-path "/" server-id "/networkUsage?" )
-               :body (if-not (and (nil? date-from) (nil? date-to))
-                        (let [body (if-not (nil? date-from)
-                                      {:dateFrom date-from}
-                                      {})
-                              body* (if-not (nil? date-to)
-                                            (assoc body :dateTo date-to)
-                                            body)]
-                          body*)
-                        nil)})
-    200 {:bandwidth nil :datatraffic nil}))
-
-(defn bandwidth-usage
-  [client server-id date-from date-to]
-  (:bandwidth (l/validate
-      (l/call client {:method "GET"
-               :resource (str api-path "/" server-id "/networkUsage/bandwidth?" )
-               :body (if-not (and (nil? date-from) (nil? date-to))
-                        (let [body (if-not (nil? date-from)
-                                      {:dateFrom date-from}
-                                      {})
-                              body* (if-not (nil? date-to)
-                                            (assoc body :dateTo date-to)
-                                            body)]
-                          body*)
-                        nil)})
-                        200 {:bandwidth nil})))
-
-(defn datatraffic-usage
-  [client server-id date-from date-to]
-  (:datatraffic (l/validate
-      (l/call client {:method "GET"
-               :resource (str api-path "/" server-id "/networkUsage/datatraffic?" )
-               :body (if-not (and (nil? date-from) (nil? date-to))
-                        (let [body (if-not (nil? date-from)
-                                      {:dateFrom date-from}
-                                      {})
-                              body* (if-not (nil? date-to)
-                                            (assoc body :dateTo date-to)
-                                            body)]
-                          body*)
-                        nil)})
-                        200 {:datatraffic nil})))
-
-(defn mk-partition-scheme
-  "helper to build partition scheme object"
-  [])
+               :resource (str api-path "/" server-id "/credentials/OPERATING_SYSTEM/" user)})
+               200))
 
 (defn get-init-root-password
   "retrieve initial rootpassword"
+  [client server]
+  (when-let [user (get-os-user client server "root")]
+    (:password user)))
+
+
+(defn reboot
   [client server-id]
-  (:rootPassword
   (l/validate
+      (l/call client {:method "POST"
+               :resource (str api-path "/" server-id "/powerCycle")})
+                202))
+
+(defn open-interfaces
+  [client server-id]
+  (l/validate
+      (l/call client {:method "POST"
+               :resource (str api-path "/" server-id "/networkInterfaces/open")})
+               2004))
+
+(defn close-interfaces
+  [client server-id]
+  (l/validate
+      (l/call client {:method "POST"
+               :resource (str api-path "/" server-id "/networkInterfaces/close")})
+                204 ))
+
+(defn list-jobs
+    [client server & {:keys [offset limit] :as query-params}]
+    (l/validate
       (l/call client {:method "GET"
-               :resource (str api-path "/" server-id "/rootPassword")})
-               200 {:rootPassword nil})))
+                      :resource (str api-path "/servers/" server "/jobs")
+                      :query-params (into {} (remove (comp nil? second) query-params))})
+       200 {:servers nil :_metadata {:error true}}))
 
+(defn list-all-jobs
+    [client & {:keys [batch-size] :or {batch-size 50}}]
+    (let [jobs (loop [js []
+                        offset 0]
+          (let [js-batch (leaseweb.v2.server/list-jobs client :limit batch-size :offset offset)]
+            (if (= (count (:jobs js-batch)) batch-size)
+                (recur (concat js (:jobs js-batch)) (+ offset batch-size))
+                (concat js (:jobs js-batch)))))]
+        {:jobs jobs :_metadata {:totalCount (count jobs) :offset 0}}))
 
+(defn bandwidth-usage
+  [client server-id & {:keys [from to granularity aggregation] :as query-params}]
+    (l/validate
+      (l/call client {:method "GET"
+                      :resource (str api-path "/servers/" server-id "/metrics/bandwidth")
+                      :query-params (into {} (remove (comp nil? second) query-params))}) 200))
+
+(defn datatraffic-usage
+  [client server-id & {:keys [from to granularity aggregation] :as query-params}]
+    (l/validate
+      (l/call client {:method "GET"
+               :resource (str api-path "/servers/" server-id "/metrics/datatraffic")
+               :query-params (into {} (remove (comp nil? second) query-params))}) 200))
+
+;; to be updated (need to update also client)
 
 (defn install
   "Install a serveur"
@@ -154,15 +142,14 @@
       (let [res (l/call client {:method "POST"
                :resource (str api-path "/" server-id "/install" )
                :body  {:osId os-id
-                      :hdd hdd
-                      :raidLevel raid-level
-                      :numberDisks number-disks}})]
+                       :hdd hdd
+                       :raidLevel raid-level
+                       :numberDisks number-disks}})]
             (if (not (= 404 (:status res)))
               res
               (do
                 (log/error "[LSW]" (:body res))
-                res)))
-                200 "error"))
+                res))) 200 "error"))
 
 (defn install-status
   [client server-id]
@@ -175,10 +162,3 @@
                               :description "unknown"
                               :serverPackId "unknown"
                               :serverName "unknown" }})))
-
-(defn reboot
-  [client server-id]
-  (l/validate
-      (l/call client {:method "POST"
-               :resource (str api-path "/" server-id "/powerCycle")})
-                202 ))
